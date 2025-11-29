18a1a41dd0bd362a2648a80236401a49ccbdf530
Omar Essam Salah <omarsalah.business063@gmail.com>
2025-11-29 23:17:41 +0200

    Update 2025_03_07_144147_create_admins_table.php

--- database/migrations/2025_03_07_144147_create_admins_table.php
+++ database/migrations/2025_03_07_144147_create_admins_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('password');
+            $table->hash('password');
             $table->timestamps();
         });
     }
